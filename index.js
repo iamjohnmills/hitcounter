283a316afc9d624686c8937be26439a96628e8f0
John Mills <jpm23456@gmail.com>
2021-02-28 11:58:35 -0600

    fix: port

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+let hits = 0;
+app.get('/', function (req, res) {
+  hits++;
+  res.send(hits.toString());
+})
+app.listen(port);
