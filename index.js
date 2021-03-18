06173107f59b184945868176f0db567b40249d94
John Mills <jpm23456@gmail.com>
2021-03-18 11:34:51 -0500

    store hits in a file

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
-let hits = 0;
-app.get('/', function (req, res) {
-  hits++;
-  res.send(hits.toString());
-})
-app.listen(port);
+
+(async () => {
+  const getHitsData = () => {
+    const hits_file = 'hits.txt'
+    if (!fs.existsSync(hits_file)) {
+      fs.writeFileSync(hits_file, '0');
+    }
+    let hits;
+    hits = parseInt(fs.readFileSync(hits_file, 'utf8')) + 1;
+    fs.writeFileSync(hits_file, hits.toString());
+    return parseInt(fs.readFileSync(hits_file, 'utf8'));
+  }
+  let hits = await getHitsData();
+
+  app.get('/', function (req, res) {
+    hits++;
+    res.send(hits.toString());
+  })
+
+  app.listen(port);
+})();
