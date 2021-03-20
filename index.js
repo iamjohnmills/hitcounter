ec6c259675cdd2e8b4e45c0667af916ab3f8985a
John Mills <jpm23456@gmail.com>
2021-03-20 11:12:46 -0500

    added mongo db to store hits

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
-
 (async () => {
-  const getHitsData = () => {
-    const hits_file = 'hits.txt'
-    if (!fs.existsSync(hits_file)) {
-      fs.writeFileSync(hits_file, '0');
-    }
+  const getHitsData = async () => {
     let hits;
-    hits = parseInt(fs.readFileSync(hits_file, 'utf8')) + 1;
-    fs.writeFileSync(hits_file, hits.toString());
-    return parseInt(fs.readFileSync(hits_file, 'utf8'));
+    const mongo_username = process.env.MONGO_DB_USER;
+    const mongo_password = process.env.MONGO_DB_PASSWORD;
+    const uri = 'mongodb+srv://' + mongo_username + ':' + mongo_password + '@cluster0.zioug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+      await client.connect();
+      const database = client.db('hitcounter');
+      const collection = database.collection('hits');
+      const query = {};
+      const options = {'total': 1};
+      const total = await collection.findOne(query, options);
+      await collection.updateOne(query, {$set: {total: total.total + 1 }});
+      const total_updated = await collection.findOne(query, options);
+      hits = total_updated.total;
+    } catch(error) {
+      hits = 10000004;
+    } finally {
+      client.close();
+      return hits;
+    }
   }
-  let hits = await getHitsData();
-
-  app.get('/', function (req, res) {
-    hits++;
+  app.get('/', async function (req, res) {
+    let hits = await getHitsData();
     res.send(hits.toString());
   })
-
   app.listen(port);
 })();
